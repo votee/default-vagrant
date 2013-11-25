6a3f1ae60ba55493952a808fed442b25d947ad46
Jacky Chan <jhkchan3@gmail.com>
2013-11-25 15:53:10 +0800

    fixes on dependencies follow coding standards

--- puppet/manifests/app/php.pp
+++ puppet/manifests/app/php.pp
@@ -11,11 +11,6 @@ class app::php {
         command => "/bin/bash -c 'cd /srv/www/vhosts/$vhost.dev && /usr/bin/php app/console cache:clear --env=dev'",
     }
 
-    exec {"create-doctrine-schema":
-        require => Package["php5-cli"],
-        command => "/bin/bash -c 'cd /srv/www/vhosts/$vhost.dev && /usr/bin/php app/console doctrine:schema:create'",
-    }
-
     if 'nginx' == $webserver {
         include app::php::fpm
     }
