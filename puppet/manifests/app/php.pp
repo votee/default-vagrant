820c8531e1aaafb4241bfb822fb5fbe99fcb297d
Jacky Chan <jhkchan3@gmail.com>
2014-02-28 16:40:54 +0800

    support phpredis

--- puppet/manifests/app/php.pp
+++ puppet/manifests/app/php.pp
@@ -1,7 +1,18 @@
 import 'php/*'
 
 class app::php {
-    package {["php5", "php5-cli", "php5-dev", "php-apc", "php5-mysql", "php5-intl", "php5-curl", "php5-xdebug"]:
+    exec {"add-php-apt-repository":
+        require => Package["python-software-properties"],
+        command => "add-apt-repository ppa:ufirst/php",
+    }
+
+    exec {"apt-update-php":
+        require => Exec["add-apt-repository"],
+        command => "/usr/bin/apt-get update",
+    }
+
+    package {["php5", "php5-cli", "php5-dev", "php-apc", "php5-mysql", "php5-intl", "php5-curl", "php5-xdebug", "php5-redis"]:
+        require => Exec["apt-update-php"],
         ensure => present,
         notify => Service[$webserverService],
     }
