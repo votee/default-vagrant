9331ca1d9a0ca49868ac1a498ff2675b418c2c78
Jacky Chan <jhkchan3@gmail.com>
2021-06-07 10:21:33 +0800

    upgrade to redis v6

--- puppet/modules/app/manifests/redis.pp
+++ puppet/modules/app/manifests/redis.pp
@@ -1,5 +1,16 @@
 class app::redis {
+    exec {"add-redis-repository":
+        require => Package["python-software-properties"],
+        command => "add-apt-repository ppa:redislabs/redis",
+    }
+
+    exec {"apt-update-redis":
+        require => Exec["add-redis-repository"],
+        command => "/usr/bin/apt-get update",
+    }
+
     package {["redis-server"]:
+        require => Exec["apt-update-redis"],
         ensure => present,
     }
 }
